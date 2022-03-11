f5488a436f1c2872def943f925115e8783df89e0
zmckevitt <zack.mckevitt@gmail.com>
2022-03-10 21:27:28 -0700

    push

--- ntp_client.c
+++ ntp_client.c
@@ -13,10 +13,12 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
 #include <time.h>
 
-#define PORT 123
-#define IP_ADDR "216.239.35.0"
+#define PORT 8080
+#define IP_ADDR "127.0.0.1"
+// #define IP_ADDR "216.239.35.0"
 
 // leap indicator
 #define LI 0
@@ -58,8 +60,6 @@ struct NTP_packet {
 
 void initPacket(struct NTP_packet* packet) {
     
-    memset(packet, 0, sizeof(struct NTP_packet));
-
     // initialize leap indicator, version number, and packet mode
     uint8_t init_li_vn_m = 0;
     init_li_vn_m |= LI << 6;
@@ -67,6 +67,28 @@ void initPacket(struct NTP_packet* packet) {
     init_li_vn_m |= MODE;
 
     packet->li_vn_m = init_li_vn_m;
+}
+
+void printPacket(struct NTP_packet packet) {
+
+    printf("--------NTP Packet--------\n");
+    printf("Leap Indicator:      %u\n", (packet.li_vn_m >> 6) & 3);
+    printf("Version Number:      %u\n", (packet.li_vn_m >> 3) & 7);
+    printf("Mode:                %u\n", (packet.li_vn_m & 7));
+    printf("Stratum:             %u\n", packet.stratum);
+    printf("Poll:                %u\n", packet.poll);
+    printf("Precision:           %u\n", packet.precision);
+    printf("Root Delay:          %u\n", packet.root_delay);
+    printf("Root Dispersion:     %u\n", packet.root_dispersion);
+    printf("Ref Identifier:      %u\n", (packet.ref_identifier));
+    printf("Ref timestamp (s):   %u\n", (packet.ref_timestamp_s));
+    printf("Ref timestamp (f):   %u\n", (packet.ref_timestamp_f));
+    printf("Org timestamp (s):   %u\n", (packet.origin_timestamp_s));
+    printf("Org timestamp (f):   %u\n", (packet.origin_timestamp_f));
+    printf("Rec timestamp (s):   %u\n", (packet.rec_timestamp_s));
+    printf("Rec timestamp (f):   %u\n", (packet.rec_timestamp_f));
+    printf("Trans timestamp (s): %u\n", (packet.trans_timestamp_s));
+    printf("Trans timestamp (f): %u\n", (packet.trans_timestamp_f));
 }
 
 int main(int argc, char const *argv[]) {
@@ -94,17 +116,23 @@ int main(int argc, char const *argv[]) {
     }
 
     struct NTP_packet packet = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    memset(&packet, 0, sizeof(struct NTP_packet));
+
     initPacket(&packet);
     
     while(1) {
         
         printf("Writing to socket...\n");
         n = write( sock, ( char* ) &packet, sizeof(struct NTP_packet ) );
-
+        printf("Reading from socket...\n");
         n = read( sock, ( char* ) &packet, sizeof(struct NTP_packet ) );
         
-        // T4 - capture immediately
-        uint32_t rec_time = time(0);
+        // T4
+        // record time of message receive
+        struct timeval r_rec;
+        gettimeofday(&r_rec, NULL);
+        uint32_t rec_time_s = r_rec.tv_sec;
+        uint32_t rec_time_f = r_rec.tv_usec;
 
         // T1
         uint32_t o_time_s = ntohl(packet.origin_timestamp_s);
@@ -119,11 +147,7 @@ int main(int argc, char const *argv[]) {
         uint32_t t_time_f = ntohl(packet.trans_timestamp_f);
 
         printf("RESPONSE FROM SERVER:\n");
-        printf("Stratum: %d\n", packet.stratum);
-        printf("Poll: %d\n", packet.poll);
-        printf("Precision: %d\n", packet.precision);
-        printf("Root Delay: %d\n", packet.root_delay);
-        printf("Reference Identifier: %d\n", packet.ref_identifier);
+        printPacket(packet);
 
         time_t origin_time = (time_t) (ntohl(packet.trans_timestamp_s) - NTP_TIMESTAMP_DELTA);
         printf("Origin Time %s", ctime((const time_t*) &origin_time));
@@ -137,16 +161,23 @@ int main(int argc, char const *argv[]) {
 
         // initPacket(&packet);
 
-        packet.li_vn_m = 35;
+        initPacket(&packet);
 
         packet.origin_timestamp_s = t_time_s;
         packet.origin_timestamp_f = t_time_f;
 
-        packet.rec_timestamp_s = rec_time;
-        packet.rec_timestamp_f = 0;
+        packet.rec_timestamp_s = rec_time_s;
+        packet.rec_timestamp_f = rec_time_f;
         
-        packet.trans_timestamp_s = time(0);
-        packet.trans_timestamp_f = 0;
+
+        // set transmit time to now
+        struct timeval t_trans;
+        gettimeofday(&t_trans, NULL);
+        packet.trans_timestamp_s = t_trans.tv_sec;
+        packet.trans_timestamp_f = t_trans.tv_usec;
+
+        printf("Transmitting at %u seconds\n", (uint32_t)(t_trans.tv_sec));
+        printf("Transmitting at %u useconds\n", (uint32_t)(t_trans.tv_usec));
     }
     
     return 0;
--- ntp_server.c
+++ ntp_server.c
@@ -1,56 +1,160 @@
+//////////////////////////////////////////////////////
+//
+// NTP Server - CSCI 5673
+//
+// Sources:
+// - https://lettier.github.io/posts/2016-04-26-lets-make-a-ntp-client-in-c.html
+// - https://www.geeksforgeeks.org/socket-programming-cc/
+// - Personal repo for code from CSCI 5273 PA1
+//
+//////////////////////////////////////////////////////
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
+
 #define PORT 8080
 
-int main(int argc, char const *argv[]) {
+// leap indicator
+#define LI 0
+
+// version number
+#define VN 4
+
+// packet mode
+#define MODE 3
+
+#define NTP_TIMESTAMP_DELTA 2208988800
+
+struct NTP_packet {
     
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-       
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-       
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    uint8_t li_vn_m;
+    uint8_t stratum;
+    uint8_t poll;
+    uint8_t precision;
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-       
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
+    uint32_t root_delay;
+    uint32_t root_dispersion;
+    uint32_t ref_identifier;
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    // seconds and fractional seconds
+    uint32_t ref_timestamp_s;
+    uint32_t ref_timestamp_f;
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+    uint32_t origin_timestamp_s;
+    uint32_t origin_timestamp_f;
 
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    uint32_t rec_timestamp_s;
+    uint32_t rec_timestamp_f;
+
+    uint32_t trans_timestamp_s;
+    uint32_t trans_timestamp_f;
+};
+
+void initPacket(struct NTP_packet* packet) {
     
-    return 0;
+    // initialize leap indicator, version number, and packet mode
+    uint8_t init_li_vn_m = 0;
+    init_li_vn_m |= LI << 6;
+    init_li_vn_m |= VN << 3;
+    init_li_vn_m |= MODE;
+
+    packet->li_vn_m = init_li_vn_m;
+}
+
+
+// open UDP socket
+int open_listenfd(int port)  {
+    int listenfd, optval=1;
+    struct sockaddr_in serveraddr;
+  
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        return -1;
+
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+                   (const void *)&optval , sizeof(int)) < 0)
+        return -1;
+
+    /* listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    if (bind(listenfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
+        return -1;
+
+    return listenfd;
+} 
+
+
+int main(int argc, char **argv) {
+
+    int listenfd, *connfdp, port, clientlen=sizeof(struct sockaddr_in), n, len;
+    struct sockaddr_in clientaddr;
+    pthread_t tid;
+    struct NTP_packet packet;
+
+    len = sizeof(clientaddr);
+    listenfd = open_listenfd(PORT);
+
+    while(1) {
+        
+        n = recvfrom(listenfd, (char*) &packet, sizeof(struct NTP_packet),
+                     0, (struct sockaddr*) &clientaddr, &len);
+
+        // T4
+        // record time of message receive
+        struct timeval t_rec;
+        gettimeofday(&t_rec, NULL);
+        uint32_t rec_time_s = t_rec.tv_sec;
+        uint32_t rec_time_f = t_rec.tv_usec;
+
+        // T1
+        uint32_t o_time_s = ntohl(packet.origin_timestamp_s);
+        uint32_t o_time_f = ntohl(packet.origin_timestamp_f);
+
+        // T2
+        uint32_t r_time_s = ntohl(packet.rec_timestamp_s);
+        uint32_t r_time_f = ntohl(packet.rec_timestamp_f);
+
+        // T3
+        uint32_t t_time_s = packet.trans_timestamp_s;
+        uint32_t t_time_f = packet.trans_timestamp_f;
+
+        printf("Received client message.\n");
+
+        initPacket(&packet);
+
+        // update origin time to client's transmit time
+        packet.origin_timestamp_s = t_time_s;
+        packet.origin_timestamp_f = t_time_f;
+
+        // update receive time to server's receive timestamp
+        packet.rec_timestamp_s = rec_time_s + NTP_TIMESTAMP_DELTA;
+        packet.rec_timestamp_f = (4294*(rec_time_f)) + ((1981*(rec_time_f))>>11);//rec_time_f;
+
+        // set transmit time to now
+        struct timeval t_trans;
+        gettimeofday(&t_trans, NULL);
+        packet.trans_timestamp_s = t_trans.tv_sec + NTP_TIMESTAMP_DELTA;
+        packet.trans_timestamp_f = (4294*(t_trans.tv_usec)) + ((1981*(t_trans.tv_usec))>>11);//t_trans.tv_usec;
+
+        printf("SECONDS SINCE 1970: %u\n", packet.rec_timestamp_s);
+
+        n = sendto(listenfd, (char*) &packet, sizeof(struct NTP_packet), 
+                   0, (const struct sockaddr*)&clientaddr, len);
+
+        if(n<0)
+            perror("Write failed\n");
+
+        printf("Wrote to client.\n");
+    }
 }
